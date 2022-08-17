5e336160e782a1311a751ef42d58af23b4493b73
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-17 20:48:41 +0530

    Update checker.c
    testing purpose only

--- checker.c
+++ checker.c
@@ -26,7 +26,7 @@ enum State checkWithinRange(char* parameter, float value, float min, float max)
   {
     currState = LOW_WARNING;
   }*/
-  else if ((value >(max - max*WARNING_TOLERANCE)) && (value <= max))
+  if ((value >(max - max*WARNING_TOLERANCE)) && (value <= max))
   {
     currState = HIGH_WARNING;
   }
