54bb0956abb080eeab389fe8bdfc18586c998e44
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-17 17:29:14 +0530

    Update checker.c
    Adding more test cases and removing duplication of code

--- checker.c
+++ checker.c
@@ -1,21 +1,61 @@
 #include <stdio.h>
 #include <assert.h>
+#define TEMP_MIN        0
+#define TEMP_MAX        45
+#define SOC_MIN         20
+#define SOC_MAX         80
+#define CHARGERATE_MAX  0.8
+int checkWithinRange(float value, float min, float max);
+int checkLessThanMax(float value, float max);
+int batteryIsOk(float temperature, float soc, float chargeRate);
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
+int checkWithinRange(float value, float min, float max)
+{
+  if ((value < min) || (value > max))
+  {
     return 0;
   }
-  return 1;
+  else
+  {
+    return 1;
+  }   
+}
+
+int checkLessThanMax(float value, float max)
+{
+  if (value > max)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }   
+}
+
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  int isTempOk = 0;
+  int isSocOk = 0;
+  int isChargeRateOk = 0;
+  isTempOk       = (temperature, TEMP_MIN, TEMP_MAX);
+  isSocOk        = (soc, SOC_MIN, SOC_MAX);
+  isChargeRateOk = (chargeRate, CHARGERATE_MAX); 
+  return (isTempOk && isSocOk && isChargeRateOk);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  /*Checking temperature range*/
+  assert(!batteryIsOk(TEMP_MIN-1, 70, 0.7));
+  assert(batteryIsOk(TEMP_MIN+1, 70, 0.7));
+  assert(!batteryIsOk(TEMP_MAX+1, 70, 0.7));
+
+  /*Checking SOC range*/
+    assert(!batteryIsOk(50, SOC_MIN-1, 0));
+    assert(batteryIsOk(50, SOC_MIN+1, 0));
+    assert(!batteryIsOk(50, SOC_MAX+1, 0));
+  
+  /*Checking chargeRate range*/
+  assert(batteryIsOk(50, 70, CHARGERATE_MAX-0.1));
+  assert(batteryIsOk(50, 70, CHARGERATE_MAX+0.1));
 }
